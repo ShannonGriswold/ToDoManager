6f9aad7be9f4a5f5970ef784a8a9f1888aa5c49f
Mollie Ackerman <molliesa2@gmail.com>
2025-11-05 14:36:12 -0500

    search and print functionality added

--- todo.hs
+++ todo.hs
@@ -1,10 +1,21 @@
 add list toAdd = list ++ [toAdd]
 
-printTask list taskInput = 
-    list
 
-search list taskInput = 
-    list
+printTask (x:xs) = do 
+    putStrLn $ "\t" ++ x
+    printTask xs
+
+printTask [] = return ();
+
+
+search (x:xs) taskInput = do
+    if x == taskInput then putStrLn $ "Found " ++ taskInput 
+    else  search xs taskInput
+
+search [] taskInput = putStrLn $ "Could not find " ++ taskInput
+
+
+    
 
 task list = do
     putStrLn "Below are the options:"
@@ -22,16 +33,15 @@ task list = do
             task newList
 
         else if input == "print" then do
-            putStrLn "in print"
-            taskInput <- getLine
-            let newList = printTask list taskInput
-            task newList
+            putStrLn "Here are your tasks: "
+            printTask list 
+            task list
 
         else if input == "search" then do
-            putStrLn "in search"
-            taskInput <- getLine
-            let newList = search list tail
-            task newList
+            putStrLn "Enter task to search"
+            taskInput <- getLine 
+            search list taskInput
+            task list
             
         else do
             putStrLn "Error"
